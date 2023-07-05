85b788e64a41a4a17f722db9e6b9c26d4df4aa03
unknown <Admin@IMECML4KTSQD2.ad.uniandes.edu.co>
2023-07-05 10:52:28 -0500

    add verification to avoid run again previus scenarios

--- src/matlab/opensees_analisis_CurvasRelVacios.m
+++ src/matlab/opensees_analisis_CurvasRelVacios.m
@@ -65,7 +65,9 @@ for corte_index = 1:length(cortes)
     corte = cortes(corte_index);
     
     tmp_folder = join([current_path, '\..\..\temp\']);
-
+    if ~exist( tmp_folder, "dir")
+        mkdir( tmp_folder)
+    end
     tmp_earthquake_file = join([current_path, '\..\..\temp\', earth_quake_file]);
     copyfile(template_earthquake_file, tmp_earthquake_file) 
     copyfile(opensees_executable_file, tmp_folder)
@@ -78,138 +80,145 @@ for corte_index = 1:length(cortes)
     opensees_arg = [opensees_executable_file, ' '... 
                     tmp_file];                  
     clear opensees_command
-    
-    
+    folder_to_test = tmp_folder + "\*.out", opensees_results_folder + "\corte_" + string(corte);
+    if exist(folder_to_test, "dir")
 
-    %% PRESIÓN DE PORORS, e, ESFUEROS TOTALES Y ESFUERZOS EFECTIVOS
-    
-    pporos = ones((suelo_0.y_tamano/suelo_0.dy) + 1, (suelo_0.x_tamano/suelo_0.dx) + 1);
-    for pporos_index = 1:( suelo_0.y_tamano / suelo_0.dy ) + 1
-        pporos(pporos_index, :) = cellfun(@(x) gammaw * ( NF - ( 1 - pporos_index) ) .* x , num2cell(pporos(pporos_index, :)));
-    end
-    
-    esfT = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);       % Matriz Esfuerzos Totales
-    esfeff = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);     % Matriz Esfuerzos Effectivos
-%     suelo_0.gsat = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);     % Matriz gamma (asumido condición parcialmente saturado)
-    
-    vIP = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-%     Rho = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-%     suelo_0.nu = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-%     cohesion = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-    %% Gmax 
-    
-    % Gmax_pp en MPa                       
-    % Gmax_pp=(16.91.*relv.^-1.728);
-    
-    %%% Gmax_final
-
-    vK0 = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);          % Matriz con los valores de K0 para cada nodo. Ecuación de Braja Das.
-    vSigma0 = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-%     suelo_0.Gmax_final = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-%     Vs = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-%     suelo_0.gammaref = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-    esfT = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-    esfeff = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-    relv = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
-    temp = [];    
-    temp_hab = [];    
-
-    for i = 1:( suelo_0.y_tamano / suelo_0.dy) + 1
-%         i = 1;
-        if i == 1
-            esfT( i, :) = 0;
-        else
-            esfT( i, :) = esfT( i - 1, :) + suelo_0.gsat( i - 1, :) * abs( suelo_0.dy);
+        %% PRESIÓN DE PORORS, e, ESFUEROS TOTALES Y ESFUERZOS EFECTIVOS
+        
+        pporos = ones((suelo_0.y_tamano/suelo_0.dy) + 1, (suelo_0.x_tamano/suelo_0.dx) + 1);
+        for pporos_index = 1:( suelo_0.y_tamano / suelo_0.dy ) + 1
+            pporos(pporos_index, :) = cellfun(@(x) gammaw * ( NF - ( 1 - pporos_index) ) .* x , num2cell(pporos(pporos_index, :)));
         end
         
-        esfeff(i, :) = esfT(i, :) - pporos(i, :); 
-
-        s1 = seteyValue(s1, esfeff(i,:));
-        s2 = seteyValue(s2, esfeff(i,:));
-        s3 = seteyValue(s3, esfeff(i,:));
-        s4 = seteyValue(s4, esfeff(i,:));
-        s5 = seteyValue(s5, esfeff(i,:));
-        s6 = seteyValue(s6, esfeff(i,:));
-        s7 = seteyValue(s7, esfeff(i,:));
-        s8 = seteyValue(s8, esfeff(i,:));
+        esfT = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);       % Matriz Esfuerzos Totales
+        esfeff = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);     % Matriz Esfuerzos Effectivos
+    %     suelo_0.gsat = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);     % Matriz gamma (asumido condición parcialmente saturado)
         
-        temp = suelo_0.vLL(i,:) <= s1.LL;
-        suelo_0 = vaciosXNodo( suelo_0, i, s1.seteyValue( esfeff( i, :)).ey .* temp );
-        suelo_0 = limitePlastico( suelo_0, i, s1.LP .* temp );
-
-        temp = interp1( [ s1.LL; s2.LL; s3.LL; s4.LL; s5.LL; s6.LL; s7.LL; s8.LL], ...
-               [ s1.seteyValue( esfeff( i, 1)).ey; ...
-               s2.seteyValue( esfeff( i, 1)).ey; ...
-               s3.seteyValue( esfeff( i, 1)).ey; ...
-               s4.seteyValue( esfeff( i, 1)).ey; ...
-               s5.seteyValue( esfeff( i, 1)).ey; ...
-               s6.seteyValue( esfeff( i, 1)).ey; ...
-               s7.seteyValue( esfeff( i, 1)).ey; ...
-               s8.seteyValue( esfeff( i, 1)).ey], ...
-               suelo_0.vLL( i, :));
-        suelo_0 = vaciosXNodo( suelo_0, i, temp);
-
-        temp = interp1([ s1.LL; s2.LL; s3.LL; s4.LL; ...
-               s5.LL; s6.LL; s7.LL; s8.LL], ...
-               [ s1.LP; s2.LP; s3.LP; s4.LP; s5.LP; ...
-               s6.LP; s7.LP; s8.LP], suelo_0.vLL( i, :));
-        suelo_0 = limitePlastico( suelo_0, i, temp);
-
-        suelo_0.relv = fillmissing(suelo_0.relv, 'constant', s8.seteyValue( esfeff( i, :)).ey);
-        suelo_0.vLP = fillmissing(suelo_0.vLP, "constant", s8.LP);
-
-        vIP(i, :) = suelo_0.vLL( i, :) - suelo_0.vLP( i, :);
-
-        comp_0 = vIP( i, :) <= 40;
-        comp_1 = vIP( i, :) > 0;
-        temp =  comp_0 .* comp_1;
-        vK0( i, :) = (0.4 + 0.007 * ( vIP( i, :))) .* temp;
+        vIP = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+    %     Rho = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+    %     suelo_0.nu = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+    %     cohesion = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+        %% Gmax 
         
-        temp = vK0( i, :) == 0;
-        vK0( i, :) = vK0( i, :) + (0.68 + 0.001 * ( vIP( i, :) - 40)) .* temp; 
+        % Gmax_pp en MPa                       
+        % Gmax_pp=(16.91.*relv.^-1.728);
         
-        suelo_0.gsat(i, :) = ( gammaw * ( suelo_0.relv(i, :) + GS)) ./ (1 + suelo_0.relv( i, :));
-
-        vSigma0( i, :) = (1/3) * (esfeff( i, :) + 2 * vK0( i, :) .* esfeff( i, :));
-
-        suelo_0.Gmax_final( i, :) = ( 16.91 * suelo_0.relv( i, :) .^ ( -1.728)) .* ( vSigma0( i, :) / 50) .^ ( 0.5);
-
-        temp = suelo_0.vLL( i, :) <= 95.9;                                      % LL para s1 gammaref.    
-        suelo_0.gammaref( i, :) = s1.gammaref * temp;
-        
-        comp_0 = suelo_0.vLL( i, :) > 95.9;              % LL para s8 gammaref.
-        comp_1 = suelo_0.vLL( i, :) <= 370.8;
-        temp = comp_0 .* comp_1;
-        temp_comp = suelo_0.gammaref( i, :) == 0; 
-        suelo_0.gammaref( i, :) = suelo_0.gammaref( i, :) + ( 6.77 * ( 10^-5) * suelo_0.vLL( i, :) - 0.0061) .* temp .* temp_comp;
-        
-        temp = suelo_0.vLL( i, :) > 370.8;
-        temp_comp = suelo_0.gammaref( i, :) == 0; 
-        suelo_0.gammaref( i, :) = suelo_0.gammaref( i, :) + s8.gammaref * ( temp_comp .* temp);
-
-        suelo_0.Rho( i, :) = suelo_0.gsat( i, :) ./ gravedad;      % Densidad del suelo en Mg/m3
-
-        suelo_0.Vs( i, :) = sqrt( ( suelo_0.Gmax_final( i, :) * 1E6) ./ ( suelo_0.Rho( i, :) * 1000));
-
-        suelo_0.cohesion( i, :) = repmat( 95 , [ 1, (suelo_0.x_tamano/suelo_0.dx) + 1]);
-        suelo_0.nu( i, :) = repmat( 0.45, [ 1, (suelo_0.x_tamano/suelo_0.dx) + 1]);
-    end
+        %%% Gmax_final
     
-    % cambio de directorio de trabajo a corrida de simulacion
-    cd(tmp_folder)
-    opensees_command = join(opensees_arg);
-    % se corre simulacion
-    Status = system(opensees_command);
+        vK0 = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);          % Matriz con los valores de K0 para cada nodo. Ecuación de Braja Das.
+        vSigma0 = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+    %     suelo_0.Gmax_final = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+    %     Vs = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+    %     suelo_0.gammaref = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+        esfT = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+        esfeff = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+        relv = zeros(( suelo_0.y_tamano / suelo_0.dy) + 1, ( suelo_0.x_tamano / suelo_0.dx) + 1);
+        temp = [];    
+        temp_hab = [];    
+    
+        for i = 1:( suelo_0.y_tamano / suelo_0.dy) + 1
+    %         i = 1;
+            if i == 1
+                esfT( i, :) = 0;
+            else
+                esfT( i, :) = esfT( i - 1, :) + suelo_0.gsat( i - 1, :) * abs( suelo_0.dy);
+            end
+            
+            esfeff(i, :) = esfT(i, :) - pporos(i, :); 
+    
+            s1 = seteyValue(s1, esfeff(i,:));
+            s2 = seteyValue(s2, esfeff(i,:));
+            s3 = seteyValue(s3, esfeff(i,:));
+            s4 = seteyValue(s4, esfeff(i,:));
+            s5 = seteyValue(s5, esfeff(i,:));
+            s6 = seteyValue(s6, esfeff(i,:));
+            s7 = seteyValue(s7, esfeff(i,:));
+            s8 = seteyValue(s8, esfeff(i,:));
+            
+            temp = suelo_0.vLL(i,:) <= s1.LL;
+            suelo_0 = vaciosXNodo( suelo_0, i, s1.seteyValue( esfeff( i, :)).ey .* temp );
+            suelo_0 = limitePlastico( suelo_0, i, s1.LP .* temp );
+    
+            temp = interp1( [ s1.LL; s2.LL; s3.LL; s4.LL; s5.LL; s6.LL; s7.LL; s8.LL], ...
+                   [ s1.seteyValue( esfeff( i, 1)).ey; ...
+                   s2.seteyValue( esfeff( i, 1)).ey; ...
+                   s3.seteyValue( esfeff( i, 1)).ey; ...
+                   s4.seteyValue( esfeff( i, 1)).ey; ...
+                   s5.seteyValue( esfeff( i, 1)).ey; ...
+                   s6.seteyValue( esfeff( i, 1)).ey; ...
+                   s7.seteyValue( esfeff( i, 1)).ey; ...
+                   s8.seteyValue( esfeff( i, 1)).ey], ...
+                   suelo_0.vLL( i, :));
+            suelo_0 = vaciosXNodo( suelo_0, i, temp);
+    
+            temp = interp1([ s1.LL; s2.LL; s3.LL; s4.LL; ...
+                   s5.LL; s6.LL; s7.LL; s8.LL], ...
+                   [ s1.LP; s2.LP; s3.LP; s4.LP; s5.LP; ...
+                   s6.LP; s7.LP; s8.LP], suelo_0.vLL( i, :));
+            suelo_0 = limitePlastico( suelo_0, i, temp);
+    
+            suelo_0.relv = fillmissing(suelo_0.relv, 'constant', s8.seteyValue( esfeff( i, :)).ey);
+            suelo_0.vLP = fillmissing(suelo_0.vLP, "constant", s8.LP);
+    
+            vIP(i, :) = suelo_0.vLL( i, :) - suelo_0.vLP( i, :);
+    
+            comp_0 = vIP( i, :) <= 40;
+            comp_1 = vIP( i, :) > 0;
+            temp =  comp_0 .* comp_1;
+            vK0( i, :) = (0.4 + 0.007 * ( vIP( i, :))) .* temp;
+            
+            temp = vK0( i, :) == 0;
+            vK0( i, :) = vK0( i, :) + (0.68 + 0.001 * ( vIP( i, :) - 40)) .* temp; 
+            
+            suelo_0.gsat(i, :) = ( gammaw * ( suelo_0.relv(i, :) + GS)) ./ (1 + suelo_0.relv( i, :));
+    
+            vSigma0( i, :) = (1/3) * (esfeff( i, :) + 2 * vK0( i, :) .* esfeff( i, :));
+    
+            suelo_0.Gmax_final( i, :) = ( 16.91 * suelo_0.relv( i, :) .^ ( -1.728)) .* ( vSigma0( i, :) / 50) .^ ( 0.5);
+    
+            temp = suelo_0.vLL( i, :) <= 95.9;                                      % LL para s1 gammaref.    
+            suelo_0.gammaref( i, :) = s1.gammaref * temp;
+            
+            comp_0 = suelo_0.vLL( i, :) > 95.9;              % LL para s8 gammaref.
+            comp_1 = suelo_0.vLL( i, :) <= 370.8;
+            temp = comp_0 .* comp_1;
+            temp_comp = suelo_0.gammaref( i, :) == 0; 
+            suelo_0.gammaref( i, :) = suelo_0.gammaref( i, :) + ( 6.77 * ( 10^-5) * suelo_0.vLL( i, :) - 0.0061) .* temp .* temp_comp;
+            
+            temp = suelo_0.vLL( i, :) > 370.8;
+            temp_comp = suelo_0.gammaref( i, :) == 0; 
+            suelo_0.gammaref( i, :) = suelo_0.gammaref( i, :) + s8.gammaref * ( temp_comp .* temp);
+    
+            suelo_0.Rho( i, :) = suelo_0.gsat( i, :) ./ gravedad;      % Densidad del suelo en Mg/m3
+    
+            suelo_0.Vs( i, :) = sqrt( ( suelo_0.Gmax_final( i, :) * 1E6) ./ ( suelo_0.Rho( i, :) * 1000));
+    
+            suelo_0.cohesion( i, :) = repmat( 95 , [ 1, (suelo_0.x_tamano/suelo_0.dx) + 1]);
+            suelo_0.nu( i, :) = repmat( 0.45, [ 1, (suelo_0.x_tamano/suelo_0.dx) + 1]);
+        end
+        
+        % cambio de directorio de trabajo a corrida de simulacion
+        cd(tmp_folder)
+        opensees_command = join(opensees_arg);
+        % se corre simulacion
+        
+        Status = system(opensees_command);
+    
+        % se crea estructura de carpetas para almacenamiento de archivos
+        if ~exist( opensees_results_folder, "dir") 
+            mkdir(opensees_results_folder);
+        end
+        copyfile( tmp_folder + "\*.out", opensees_results_folder + "\corte_" + string(corte))
+        delete(tmp_folder + "\*")
+        
+        % se copian archivos relacionados con los resultados de la simulacion
+    else
+        message = "*** LA CARPETA " + ...
+                  opensees_results_folder + "\corte_" + string(corte) ...
+                  + " YA EXISTE ***";
+        disp(message);
 
-    % se crea estructura de carpetas para almacenamiento de archivos
-    if ~exist( opensees_results_folder, "dir") 
-        mkdir(opensees_results_folder);
-    end
-    copyfile( tmp_folder + "\*.out", opensees_results_folder + "\corte_" + string(corte))
-    delete(tmp_folder + "\*")
-    
-    % se copian archivos relacionados con los resultados de la simulacion
-    
+    end    
 end
 
 px = 0:suelo_0.dx:suelo_0.x_tamano;
